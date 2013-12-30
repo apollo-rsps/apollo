300404122ccf6643ddd5089e5f1a85eafb7224c2
Major- <major.emrs@gmail.com>
2013-12-30 09:53:49 +0000

    Use ordinal instead of value for Gender

--- src/org/apollo/game/model/Gender.java
+++ src/org/apollo/game/model/Gender.java
@@ -10,26 +10,12 @@ public enum Gender {
 	/**
 	 * The male gender.
 	 */
-	MALE(0),
+	MALE,
 
 	/**
 	 * The female gender.
 	 */
-	FEMALE(1);
-
-	/**
-	 * The numerical value used by the client.
-	 */
-	private final int value;
-
-	/**
-	 * Creates the gender.
-	 * 
-	 * @param value The numerical value.
-	 */
-	private Gender(int value) {
-		this.value = value;
-	}
+	FEMALE;
 
 	/**
 	 * Converts this gender to an integer.
@@ -37,7 +23,7 @@ public enum Gender {
 	 * @return The numerical value used by the client.
 	 */
 	public int toInteger() {
-		return value;
+		return ordinal();
 	}
 
 }
