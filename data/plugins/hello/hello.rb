a71d14ed55b9ca5182a3aeeeddc52b93d700bf24
Major- <major@emulate.rs>
2014-02-25 01:09:47 +0000

    Remove hello plugin.

--- data/plugins/hello/hello.rb
+++
@@ -1,8 +0,0 @@
-require 'java'
-java_import 'org.apollo.game.model.Animation'
-java_import 'org.apollo.game.model.World'
-
-on :command, :hello do |player, command|
-  player.play_animation Animation::WAVE
-  player.send_message("Hello, World!")
-end
