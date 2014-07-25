27b202b5197806bad0ebb0f390a0ff86baf76104
Major- <major.emrs@gmail.com>
2014-07-25 04:08:54 +0100

    Rename filter plugin to messaging, add broadcast command.

---
+++ data/plugins/cmd/messaging/broadcast.rb
@@ -0,0 +1,11 @@
+require_java
+
+java_import 'org.apollo.game.model.World'
+java_import 'org.apollo.game.model.entity.Player'
+
+# Adds a command to broadcast a message to every player on the server.
+on :command, :broadcast, RIGHTS_ADMIN do |player, command|
+  message = command.arguments.to_a.join(" ")
+  broadcast = "[Broadcast] #{player.get_username.capitalize}: #{message}"
+  World.world.player_repository.each { |player| player.send_message(broadcast) }
+end
