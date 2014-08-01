4b5156e7ade15326eef52e63b0bd330b77e35e60
Major- <major.emrs@gmail.com>
2014-08-01 15:17:01 +0100

    Add comments to teleport, use the conventional ' ' for strings with no interpolation

--- data/plugins/cmd/teleport/teleport.rb
+++ data/plugins/cmd/teleport/teleport.rb
@@ -1,14 +1,17 @@
 require 'java'
+
 java_import 'org.apollo.game.model.Position'
 
+# Sends the player's position.
 on :command, :pos, RIGHTS_MOD do |player, command|
-  player.send_message("You are at: #{player.position}")
+  player.send_message('You are at: #{player.position}.')
 end
 
+# Teleports the player to the specified position.
 on :command, :tele, RIGHTS_ADMIN do |player, command|
   args = command.arguments
   unless (2..3).include?(args.length)
-    player.send_message("Invalid syntax - ::tele [x] [y] [optional-z]")
+    player.send_message('Invalid syntax - ::tele [x] [y] [optional-z]')
     return
   end
 
@@ -16,5 +19,5 @@ on :command, :tele, RIGHTS_ADMIN do |player, command|
   y = args[1].to_i
   z = args.length == 3 ? args[2].to_i : player.position.height
 
-  player.teleport Position.new(x, y, z) if (0..4).include?(z)
+  player.teleport(Position.new(x, y, z) if (0..4).include?(z))
 end
