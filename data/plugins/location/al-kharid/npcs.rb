44ee0ddd5d71b1d9abc1bfff542fa79ab8e1000e
Major- <major@emulate.rs>
2014-08-08 14:49:25 +0100

    Format and add npcs in the Al-Kharid mine.

--- data/plugins/location/al-kharid/npcs.rb
+++ data/plugins/location/al-kharid/npcs.rb
@@ -16,6 +16,8 @@
 
 spawn_npc :name => :man, :x => 3276, :y => 3186
 
+# Palace guards
+
 spawn_npc :name => "Al-Kharid warrior", :x => 3283, :y => 3161 # String must be used here because the actual npc name is 'Al-Kharid warrior', and symbols don't support hyphens.
 spawn_npc :name => "Al-Kharid warrior", :x => 3285, :y => 3174
 spawn_npc :name => "Al-Kharid warrior", :x => 3286, :y => 3164
@@ -29,16 +31,23 @@ spawn_npc :name => "Al-Kharid warrior", :x => 3300, :y => 3171
 spawn_npc :name => "Al-Kharid warrior", :x => 3301, :y => 3164
 spawn_npc :name => "Al-Kharid warrior", :x => 3301, :y => 3168
 
-spawn_npc :name => :shantay_guard,     :x => 3304, :y => 3119
 spawn_npc :name => :shantay_guard_838, :x => 3301, :y => 3120
+spawn_npc :name => :shantay_guard,     :x => 3304, :y => 3119
 spawn_npc :name => :shantay_guard_838, :x => 3307, :y => 3122
+
+# Mine
+
+spawn_npc :name => :scorpion, :x => 3296, :y => 3294
+spawn_npc :name => :scorpion, :x => 3298, :y => 3280
+spawn_npc :name => :scorpion, :x => 3299, :y => 3299
+spawn_npc :name => :scorpion, :x => 3299, :y => 3309
+spawn_npc :name => :scorpion, :x => 3300, :y => 3287
+spawn_npc :name => :scorpion, :x => 3301, :y => 3305
+
 
 # Functional npcs
 
-spawn_npc :name => :gem_trader, :x => 3287, :y => 3210
-spawn_npc :name => :shantay,    :x => 3304, :y => 3124
-
-spawn_npc :name => :rug_merchant_2296, :x => 3311, :y => 3109, :face => :west
+spawn_npc :name => :gnome_pilot, :x => 3279, :y => 3213
 
 spawn_npc :name => :banker_496, :x => 3267, :y => 3164, :face => :east # TODO are these the correct banker ids?
 spawn_npc :name => :banker_497, :x => 3267, :y => 3166, :face => :east
@@ -46,9 +55,12 @@ spawn_npc :name => :banker_496, :x => 3267, :y => 3167, :face => :east
 spawn_npc :name => :banker_497, :x => 3267, :y => 3168, :face => :east
 spawn_npc :name => :banker_496, :x => 3267, :y => 3169, :face => :east
 
+spawn_npc :name => :gem_trader, :x => 3287, :y => 3210
 spawn_npc :name => :zeke,       :x => 3289, :y => 3189
-spawn_npc :name => :ranael,     :x => 3315, :y => 3163, :face => :north
-spawn_npc :name => :louie_legs, :x => 3316, :y => 3175, :face => :west
+spawn_npc :name => :shantay,    :x => 3304, :y => 3124
 
+spawn_npc :name => :rug_merchant_2296,  :x => 3311, :y => 3109, :face => :west
+spawn_npc :name => :ranael,             :x => 3315, :y => 3163, :face => :north
 spawn_npc :name => :shop_assistant_525, :x => 3315, :y => 3178, :face => :north # TODO are these the correct shop staff ids?
 spawn_npc :name => :shop_keeper_524,    :x => 3315, :y => 3180, :face => :west
+spawn_npc :name => :louie_legs,         :x => 3316, :y => 3175, :face => :west
