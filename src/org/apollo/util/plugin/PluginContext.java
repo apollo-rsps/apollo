66ab108e51c2e25db5f22513d4753f0fda9a406e
Major- <major.emrs@gmail.com>
2014-08-08 03:50:33 +0100

    Remove unused methods from PluginContext

--- src/org/apollo/util/plugin/PluginContext.java
+++ src/org/apollo/util/plugin/PluginContext.java
@@ -10,9 +10,6 @@ import org.apollo.game.event.handler.chain.EventHandlerChainGroup;
 import org.apollo.game.login.LoginListener;
 import org.apollo.game.login.LogoutListener;
 import org.apollo.game.model.World;
-import org.apollo.net.release.EventDecoder;
-import org.apollo.net.release.EventEncoder;
-import org.apollo.net.release.Release;
 
 /**
  * The {@link PluginContext} contains methods a plugin can use to interface with the server, for example, by adding
@@ -44,36 +41,6 @@ public final class PluginContext {
 	 */
 	public void addCommandListener(String name, CommandListener listener) {
 		World.getWorld().getCommandDispatcher().register(name, listener);
-	}
-
-	/**
-	 * Adds an {@link EventDecoder}.
-	 * 
-	 * @param <T> The type of decoder.
-	 * @param releaseNo The release number.
-	 * @param opcode The opcode.
-	 * @param decoder The event decoder.
-	 */
-	public <T extends Event> void addEventDecoder(int releaseNo, int opcode, EventDecoder<T> decoder) {
-		Release release = context.getRelease();
-		if (release.getReleaseNumber() == releaseNo) {
-			release.register(opcode, decoder);
-		}
-	}
-
-	/**
-	 * Adds an {@link EventEncoder}.
-	 * 
-	 * @param <E> The type of encoder.
-	 * @param releaseNo The release number.
-	 * @param event The event.
-	 * @param encoder The event encoder.
-	 */
-	public <E extends Event> void addEventEncoder(int releaseNo, Class<E> event, EventEncoder<E> encoder) {
-		Release release = context.getRelease();
-		if (release.getReleaseNumber() == releaseNo) {
-			release.register(event, encoder);
-		}
 	}
 
 	/**
