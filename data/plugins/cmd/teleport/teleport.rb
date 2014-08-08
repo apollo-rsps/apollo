77aad5ae664751f182d8c81950e06cecf90461a9
Major- <major.emrs@gmail.com>
2014-08-08 02:59:49 +0100

    Fix ::pos command.

--- data/plugins/cmd/teleport/teleport.rb
+++ data/plugins/cmd/teleport/teleport.rb
@@ -4,7 +4,7 @@ java_import 'org.apollo.game.model.Position'
 
 # Sends the player's position.
 on :command, :pos, RIGHTS_MOD do |player, command|
-  player.send_message('You are at: #{player.position}.')
+  player.send_message("You are at: #{player.position}.")
 end
 
 # Teleports the player to the specified position.
