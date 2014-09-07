e26ab7cef2ae1b273455330ddb3aa9fa145ab772
Lmctruck30 <xbigxstuffx@Ryans-Mac-mini.local>
2014-09-07 15:22:41 -0700

    Fix to cache update block
    - This is to reset the cache update block each cycle.
    - Idea to update only when a update happens didn’t show the results to
    implement on the git
    - Tested with 2000 bots with items and moving around in different
    sectors results are below.
    - Avg cycle times with reseting the cache each cycle 380ms
    - Avg cycle times with updating block only when change 370ms

--- src/org/apollo/game/sync/task/PostPlayerSynchronizationTask.java
+++ src/org/apollo/game/sync/task/PostPlayerSynchronizationTask.java
@@ -28,6 +28,7 @@ public final class PostPlayerSynchronizationTask extends SynchronizationTask {
 		player.setTeleporting(false);
 		player.setRegionChanged(false);
 		player.resetBlockSet();
+		player.setCachedAppearance(null);
 		if (!player.isExcessivePlayersSet()) {
 			player.incrementViewingDistance();
 		} else {
