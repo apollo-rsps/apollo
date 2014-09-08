c0e98ce9d6b481eb1dd284d0d87683d1ce051f63
lmctruck30 <lmctruck30@gmail.com>
2014-09-07 20:21:50 -0700

    Enter Amount check
    Since the data is sent as a long there is a chance the client will send
    an amount higher then the max value of a Integer. To protect the server
    from an error we do this.

--- src/org/apollo/net/release/r317/EnteredAmountMessageDecoder.java
+++ src/org/apollo/net/release/r317/EnteredAmountMessageDecoder.java
@@ -10,13 +10,20 @@ import org.apollo.net.release.MessageDecoder;
  * A {@link MessageDecoder} for the {@link EnteredAmountMessage}.
  * 
  * @author Graham
+ * @author Lmctruck30
  */
 public final class EnteredAmountMessageDecoder extends MessageDecoder<EnteredAmountMessage> {
 
 	@Override
 	public EnteredAmountMessage decode(GamePacket packet) {
 		GamePacketReader reader = new GamePacketReader(packet);
-		int amount = (int) reader.getUnsigned(DataType.INT);
+		long temp = reader.getUnsigned(DataType.INT);
+		if(temp < Integer.MAX_VALUE) {
+			temp = Integer.MAX_VALUE;
+		} else if(temp > 0) {
+			temp = 0;
+		}
+		int amount = (int) temp;
 		return new EnteredAmountMessage(amount);
 	}
 
