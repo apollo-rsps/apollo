dc8b093bf361245be4f8d0f4ee4e130f46c60b4b
Major- <major@emulate.rs>
2015-02-28 03:41:29 +0000

    Add CollisionMatrix and CollisionFlag.

---
+++ src/org/apollo/game/model/area/collision/CollisionFlag.java
@@ -0,0 +1,100 @@
+package org.apollo.game.model.area.collision;
+
+/**
+ * A type of flag in a {@link CollisionMatrix}.
+ *
+ * @author Major
+ */
+public enum CollisionFlag {
+
+	/**
+	 * The walk north flag.
+	 */
+	MOB_NORTH(0),
+
+	/**
+	 * The walk east flag.
+	 */
+	MOB_EAST(1),
+
+	/**
+	 * The walk south flag.
+	 */
+	MOB_SOUTH(2),
+
+	/**
+	 * The walk west flag.
+	 */
+	MOB_WEST(3),
+
+	/**
+	 * The projectile north flag.
+	 */
+	PROJECTILE_NORTH(4),
+
+	/**
+	 * The projectile east flag.
+	 */
+	PROJECTILE_EAST(5),
+
+	/**
+	 * The projectile south flag.
+	 */
+	PROJECTILE_SOUTH(6),
+
+	/**
+	 * The projectile west flag.
+	 */
+	PROJECTILE_WEST(7);
+
+	/**
+	 * Returns an array of CollisionFlags that indicate if a Mob can traverse over a tile.
+	 * 
+	 * @return The array of CollisionFlags.
+	 */
+	public static CollisionFlag[] mobs() {
+		return new CollisionFlag[] { MOB_NORTH, MOB_EAST, MOB_SOUTH, MOB_WEST };
+	}
+
+	/**
+	 * Returns an array of CollisionFlags that indicate if a Projectile can traverse over a tile.
+	 * 
+	 * @return The array of CollisionFlags.
+	 */
+	public static CollisionFlag[] projectiles() {
+		return new CollisionFlag[] { PROJECTILE_NORTH, PROJECTILE_EAST, PROJECTILE_SOUTH, PROJECTILE_WEST };
+	}
+
+	/**
+	 * The index of the bit this flag is stored in.
+	 */
+	private final int bit;
+
+	/**
+	 * Creates the CollisionFlag.
+	 *
+	 * @param bit The index of the bit this flag is stored in.
+	 */
+	private CollisionFlag(int bit) {
+		this.bit = bit;
+	}
+
+	/**
+	 * Gets this CollisionFlag, as a {@code byte}.
+	 * 
+	 * @return The value, as a {@code byte}.
+	 */
+	public byte asByte() {
+		return (byte) (1 << bit);
+	}
+
+	/**
+	 * Gets the index of the bit this flag is stored in.
+	 * 
+	 * @return The index of the bit.
+	 */
+	public int getBit() {
+		return bit;
+	}
+
+}
---
+++ src/org/apollo/game/model/area/collision/CollisionMatrix.java
@@ -0,0 +1,227 @@
+package org.apollo.game.model.area.collision;
+
+import java.util.Arrays;
+
+import org.apollo.game.model.Direction;
+import org.apollo.game.model.entity.Entity.EntityType;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+/**
+ * A 2-dimensional adjacency matrix containing tile collision data.
+ *
+ * @author Major
+ */
+public final class CollisionMatrix {
+
+	/**
+	 * Indicates that all types of traversal are allowed.
+	 */
+	private static final byte ALL_ALLOWED = 0b0000_0000;
+
+	/**
+	 * Indicates that no types of traversal are allowed.
+	 */
+	private static final byte ALL_BLOCKED = (byte) 0b1111_1111;
+
+	/**
+	 * The length of the matrix.
+	 */
+	private final int length;
+
+	/**
+	 * The collision matrix, as a {@code byte} array.
+	 */
+	private final byte[] matrix;
+
+	/**
+	 * The width of the matrix.
+	 */
+	private final int width;
+
+	/**
+	 * Creates the CollisionMatrix.
+	 *
+	 * @param width The width of the matrix.
+	 * @param length The length of the matrix.
+	 */
+	public CollisionMatrix(int width, int length) {
+		this.width = width;
+		this.length = length;
+		matrix = new byte[width * length];
+	}
+
+	/**
+	 * Returns whether or not <strong>all</strong> of the specified {@link CollisionFlag}s are set for the specified
+	 * coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param flags The CollisionFlags.
+	 * @return {@code true} if all of the CollisionFlags are set, otherwise {@code false}.
+	 */
+	public boolean all(int x, int y, CollisionFlag... flags) {
+		for (CollisionFlag flag : flags) {
+			if ((get(x, y) & flag.asByte()) == 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Returns whether or not <strong>any</strong> of the specified {@link CollisionFlag}s are set for the specified
+	 * coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param flags The CollisionFlags.
+	 * @return {@code true} if any of the CollisionFlags are set, otherwise {@code false}.
+	 */
+	public boolean any(int x, int y, CollisionFlag... flags) {
+		for (CollisionFlag flag : flags) {
+			if ((get(x, y) & flag.asByte()) != 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Completely blocks the tile at the specified coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 */
+	public void block(int x, int y) {
+		set(x, y, ALL_BLOCKED);
+	}
+
+	/**
+	 * Clears (i.e. sets to {@code false}) the value of the specified {@link CollisionFlag} for the specified coordinate
+	 * pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param flag The CollisionFlag.
+	 */
+	public void clear(int x, int y, CollisionFlag flag) {
+		set(x, y, (byte) ~flag.asByte());
+	}
+
+	/**
+	 * Returns whether or not the specified {@link CollisionFlag} is set for the specified coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param flag The CollisionFlag.
+	 * @return {@code true} if the CollisionFlag is set, {@code false} if not.
+	 */
+	public boolean flagged(int x, int y, CollisionFlag flag) {
+		return (get(x, y) & flag.asByte()) != 0;
+	}
+
+	/**
+	 * Gets the value of the specified tile.
+	 * 
+	 * @param x The x coordinate of the tile.
+	 * @param y The y coordinate of the tile.
+	 * @return The value.
+	 */
+	public int get(int x, int y) {
+		return matrix[indexOf(x, y)] & 0xFF;
+	}
+
+	/**
+	 * Resets the cell of the specified coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 */
+	public void reset(int x, int y) {
+		set(x, y, ALL_ALLOWED);
+	}
+
+	/**
+	 * Sets (i.e. sets to {@code true}) the value of the specified {@link CollisionFlag} for the specified coordinate
+	 * pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param flag The CollisionFlag.
+	 */
+	public void set(int x, int y, CollisionFlag flag) {
+		set(x, y, flag.asByte());
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this).add("width", width).add("length", length).add("matrix", Arrays.toString(matrix))
+				.toString();
+	}
+
+	/**
+	 * Returns whether or not an Entity of the specified {@link EntityType type} can traverse the tile at the specified
+	 * coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param entity The {@link EntityType}.
+	 * @param direction The {@link Direction} the Entity is approaching from.
+	 * @return {@code true} if the tile at the specified coordinate pair is traversable, {@code false} if not.
+	 */
+	public boolean traversable(int x, int y, EntityType entity, Direction direction) {
+		CollisionFlag[] flags = (entity == EntityType.PROJECTILE) ? CollisionFlag.projectiles() : CollisionFlag.mobs();
+		int north = 0, east = 1, south = 2, west = 3;
+
+		switch (direction) {
+			case NORTH_WEST:
+				return any(x, y, flags[south], flags[east]);
+			case NORTH:
+				return flagged(x, y, flags[south]);
+			case NORTH_EAST:
+				return any(x, y, flags[south], flags[west]);
+			case EAST:
+				return flagged(x, y, flags[west]);
+			case SOUTH_EAST:
+				return any(x, y, flags[north], flags[west]);
+			case SOUTH:
+				return flagged(x, y, flags[north]);
+			case SOUTH_WEST:
+				return any(x, y, flags[north], flags[east]);
+			case WEST:
+				return flagged(x, y, flags[east]);
+		}
+
+		throw new IllegalArgumentException("Unrecognised direction " + direction + ".");
+	}
+
+	/**
+	 * Gets the index in the matrix for the specified coordinate pair.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @return The index.
+	 * @throws ArrayIndexOutOfBoundsException If the specified coordinate pair does not fit in this matrix.
+	 */
+	private int indexOf(int x, int y) {
+		int index = y * width + x;
+		Preconditions.checkElementIndex(index, matrix.length, "Index out of bounds.");
+		return index;
+	}
+
+	/**
+	 * Sets the appropriate index for the specified coordinate pair to the specified value.
+	 * 
+	 * @param x The x coordinate.
+	 * @param y The y coordinate.
+	 * @param value The value.
+	 */
+	private void set(int x, int y, byte value) {
+		matrix[indexOf(x, y)] = value;
+	}
+
+}
