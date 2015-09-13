ef398dc098f4e2e26de6c65affd9d182abe65eca
Gary Tierney <gary.tierney@gmx.com>
2015-09-13 01:25:00 +0100

    Add a test around ChatMessageHandler, update imports
    Update the previous ItemOnItemVerificationHandler test to use static
    imports of PowerMockito and Assert.

--- game/src/test/org/apollo/game/message/handler/ItemOnItemVerificationHandlerTest.java
+++ game/src/test/org/apollo/game/message/handler/ItemOnItemVerificationHandlerTest.java
@@ -10,12 +10,11 @@ import org.apollo.game.model.inv.Inventory;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({Player.class, ItemDefinition.class})
@@ -28,17 +27,17 @@ public class ItemOnItemVerificationHandlerTest {
 
 	@BeforeClass
 	public static void setupTestItemDefinitions() {
-		PowerMockito.mockStatic(ItemDefinition.class);
-		Mockito.when(ItemDefinition.lookup(4151)).thenReturn(new ItemDefinition(4151));
+		mockStatic(ItemDefinition.class);
+		when(ItemDefinition.lookup(4151)).thenReturn(new ItemDefinition(4151));
 	}
 
 	@Test
 	public void testTerminateWithNoSourceItem() throws Exception {
-		Player player = PowerMockito.mock(Player.class);
+		Player player = mock(Player.class);
 		Inventory inventory = new Inventory(28);
 		inventory.set(1, new Item(4151, 1));
 
-		Mockito.when(player.getInventory()).thenReturn(inventory);
+		when(player.getInventory()).thenReturn(inventory);
 
 		ItemOnItemMessage itemOnItemMessage = new ItemOnItemMessage(BankConstants.SIDEBAR_INVENTORY_ID, 500, 1,
 				BankConstants.SIDEBAR_INVENTORY_ID, 4151, 1);
@@ -51,11 +50,11 @@ public class ItemOnItemVerificationHandlerTest {
 
 	@Test
 	public void testTerminateWithNoTargetItem() throws Exception {
-		Player player = PowerMockito.mock(Player.class);
+		Player player = mock(Player.class);
 		Inventory inventory = new Inventory(28);
 		inventory.set(1, new Item(4151, 1));
 
-		Mockito.when(player.getInventory()).thenReturn(inventory);
+		when(player.getInventory()).thenReturn(inventory);
 
 		ItemOnItemMessage itemOnItemMessage = new ItemOnItemMessage(BankConstants.SIDEBAR_INVENTORY_ID, 4151, 1,
 				BankConstants.SIDEBAR_INVENTORY_ID, 4152, 2);
