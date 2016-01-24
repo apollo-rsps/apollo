32ef5062115cf5b99be6430def2a5ffc46242d36
atomicint <ryley.kimmel@live.com>
2016-01-24 03:26:49 -0500

    Fixes issue #65, adds support for emotes/anims in dialogue
    Use block instead of lambda, add emotes.rb
    
    Proper tutorial island emote
    
    Add newline at end of emotes.rb
    
    Use block instead of lambda, fix send_generic_dialogue method to accept block

---
+++ data/plugins/dialogue/emotes.rb
@@ -0,0 +1,7 @@
+# Declares all of the possible emotes
+declare_emote(:calm, 588)
+declare_emote(:anxious, 589)
+declare_emote(:default, 591)
+
+# TODO: Properly name the rest of the facial emotes.
+# Found at http://www.rune-server.org/runescape-development/rs2-server/tutorials/518991-pi-317-player-npc-facial-dialogue-expressions.html
