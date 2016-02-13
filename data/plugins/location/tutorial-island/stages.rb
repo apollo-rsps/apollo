f83e8b3040d9f2090f17b76e13b6d6c63eb02154
Major- <major@emulate.rs>
2016-02-13 16:26:51 +0000

    Fix many issues with the Tutorial Island plugin
    Amongst other things, this:
    * Stops the player from trying to leave the Runescape Guide's house if
      they have yet to speak to him.
    * Fixes the hint icon for both the Runescape Guide and the door.
    * Ensures all files pass rubocop.
    * Generally improves the code throughout the plugin.

--- data/plugins/location/tutorial-island/stages.rb
+++ data/plugins/location/tutorial-island/stages.rb
@@ -2,15 +2,15 @@
 private
 
 # The array of stages in tutorial island.
-STAGES = []
+@stages = []
 
 # The stages that are used when interacting with the Runescape Guide.
 RUNESCAPE_GUIDE = [:not_started, :talk_to_people, :go_through_door, :runescape_guide_finished,
-                   :moving_around]
-STAGES.concat(RUNESCAPE_GUIDE)
+                   :moving_around].freeze
+@stages.concat(RUNESCAPE_GUIDE)
 
 # The stages that are used when interacting with the Survival Expert.
-SURVIVAL_EXPERT = [:given_axe, :cut_tree, :cutting_tree]
-STAGES.concat(SURVIVAL_EXPERT)
+SURVIVAL_EXPERT = [:given_axe, :cut_tree, :cutting_tree].freeze
+@stages.concat(SURVIVAL_EXPERT)
 
-quest :tutorial_island, STAGES
+quest :tutorial_island, @stages
