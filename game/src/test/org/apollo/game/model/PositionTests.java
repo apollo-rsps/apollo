47d054c38d28eca5f088e4effe19a00a1507be02
CharlesVaneenoo <charlesvaneenoo@gmail.com>
2016-02-27 11:02:15 +0100

    added test methods to PositionTests

--- game/src/test/org/apollo/game/model/PositionTests.java
+++ game/src/test/org/apollo/game/model/PositionTests.java
@@ -1,5 +1,8 @@
 package org.apollo.game.model;
 
+import org.apollo.game.model.area.RegionCoordinates;
+import org.apollo.game.model.area.Region;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,6 +18,99 @@ public final class PositionTests {
 	public void getHeight() {
 		Position position = new Position(3222, 3222, 3);
 		Assert.assertEquals(3, position.getHeight());
+	}
+
+	/**
+	 * Tests the {@link Position#getX()} method.
+	 */
+	@Test
+	public void getX() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(3222, position.getX());
+	}
+
+	/**
+	 * Tests the {@link Position#getY()} method.
+	 */
+	@Test
+	public void getY() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(3222, position.getY());
+	}
+
+	/**
+	 * Tests the {@link Position#getCentralRegionX()} method.
+	 */
+	@Test
+	public void getCentralRegionX() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(402,75, position.getCentralRegionX());
+	}
+
+	/**
+	 * Tests the {@link Position#getCentralRegionY()} method.
+	 */
+	@Test
+	public void getCentralRegionY() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(402,75, position.getCentralRegionY());
+	}
+
+	/**
+	 * Tests the {@link Position#getDistance()} method.
+	 */
+	@Test
+	public void getDistance() {
+		Position position1 = new Position(3222, 3222, 3);
+		Position position2 = new Position(3220, 3220, 3);
+		Assert.assertEquals(3, position1.getDistance(position2));
+	}
+
+	/**
+	 * Tests the {@link Position#getLocalX()} method.
+	 */
+	@Test
+	public void getLocalX() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(54, position.getLocalX());
+	}
+
+	/**
+	 * Tests the {@link Position#getLocalY()} method.
+	 */
+	@Test
+	public void getLocalY() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(54, position.getLocalY());
+	}
+
+	/**
+	 * Tests the {@link Position#getLongestDelta()} method.
+	 */
+	@Test
+	public void getLongestDelta() {
+		Position position1 = new Position(3222, 3222, 3);
+		Position position2 = new Position(3219, 3220, 3);
+		Assert.assertEquals(3, position1.getLongestDelta(position2));
+	}
+
+	/**
+	 * Tests the {@link Position#getRegionCoordinates()} method.
+	 */
+	@Test
+	public void getRegionCoordinates() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertEquals(new RegionCoordinates(396,396), position.getRegionCoordinates());
+	}
+
+	/**
+	 * Tests the {@link Position#inside()} method.
+	 */
+	@Test
+	public void inside() {
+		Position position = new Position(3222, 3222, 3);
+		Assert.assertTrue(position.inside(new Region(396,396)));
+		Assert.assertFalse(position.inside(new Region(1,1)));
 	}
 
 	/**
