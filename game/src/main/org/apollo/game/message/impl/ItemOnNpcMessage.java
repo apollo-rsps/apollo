dd1ae1c5ca9731545bc7713c78c104b5e2113ac1
Lmctruck30 <lmctruck30@gmail.com>
2016-12-03 17:20:15 -0800

    Change to final on fields
    @garyttierney change request added. Thank you.

--- game/src/main/org/apollo/game/message/impl/ItemOnNpcMessage.java
+++ game/src/main/org/apollo/game/message/impl/ItemOnNpcMessage.java
@@ -4,39 +4,54 @@ import org.apollo.net.message.Message;
 
 /**
  * A {@link Message} sent by the client when a player uses an item on a npc.
- * 
+ *
  * @author Lmctruck30
  */
 
 public final class ItemOnNpcMessage extends Message {
 
-	private int itemId;
-	
-	private int npcIndex;
-	
-	private int itemSlot;
-	
-	private int interfaceId;
-	
+	/**
+	 * Item id used on the Npc.
+	 */
+	private final int itemId;
+
+	/**
+	 * Npc index that the item was used on.
+	 */
+	private final int npcIndex;
+
+	/**
+	 * Item slot from inventory.
+	 */
+	private final int itemSlot;
+
+	/**
+	 * Interface item was used on the npc.
+	 */
+	private final int interfaceId;
+
+	/**
+	 *
+	 */
 	public ItemOnNpcMessage(int itemId, int npcIndex, int itemSlot, int interfaceId) {
 		this.itemId = itemId;
 		this.itemSlot = itemSlot;
 		this.npcIndex = npcIndex;
 		this.interfaceId = interfaceId;
 	}
-	
+
 	public int getItemId() {
 		return itemId;
 	}
-	
+
 	public int getItemSlot() {
 		return itemSlot;
 	}
-	
+
 	public int getNpcIndex() {
 		return npcIndex;
 	}
-	
+
 	public int getInterfaceId() {
 		return interfaceId;
 	}
--- game/src/main/org/apollo/game/message/impl/ReportAbuseMessage.java
+++ game/src/main/org/apollo/game/message/impl/ReportAbuseMessage.java
@@ -2,28 +2,34 @@ package org.apollo.game.message.impl;
 
 import org.apollo.net.message.Message;
 
+/**
+ * A {@link Message} sent by the client when a player reports another player.
+ *
+ * @author Lmctruck30
+ */
+
 public final class ReportAbuseMessage extends Message {
-	
+
 	private final long reported;
-	
+
 	private final int rule;
-	
+
 	private final boolean mute;
-	
+
 	public ReportAbuseMessage(long reported, int rule, boolean mute) {
 		this.reported = reported;
 		this.rule = rule;
 		this.mute = mute;
 	}
-	
+
 	public long getReported() {
 		return reported;
 	}
-	
+
 	public int getRule() {
 		return rule;
 	}
-	
+
 	public boolean getMute() {
 		return mute;
 	}
