fc718be9c964a37d3e576d43df782e91abba5826
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-02-04 16:22:51 +0100

    Test MessageHandlerChainSet.

---
+++ game/src/test/org/apollo/game/message/handler/MessageHandlerChainSetTest.java
@@ -0,0 +1,113 @@
+package org.apollo.game.message.handler;
+
+import org.apollo.game.model.World;
+import org.apollo.game.model.entity.Player;
+import org.apollo.game.util.mocks.FakeMessage;
+import org.apollo.game.util.mocks.FakeMessageHandler;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+/**
+ * @author Lesley
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Player.class)
+public final class MessageHandlerChainSetTest {
+
+	@Test
+	public void notify_byDefault_notifiesAddedHandlers() {
+		FakeMessageHandler mockMessageHandler = makeFakeMessageHandler();
+		Player stubPlayer = makePlayer();
+		FakeMessage stubMessage = makeFakeMessage();
+		MessageHandlerChainSet chain = makeChainSet();
+
+		chain.putHandler(FakeMessage.class, mockMessageHandler);
+		chain.notify(stubPlayer, stubMessage);
+
+		Player expectedPlayer = stubPlayer;
+		Player actualPlayer = mockMessageHandler.lastHandlePlayer;
+		assertEquals(expectedPlayer, actualPlayer);
+
+		FakeMessage expectedMessage = stubMessage;
+		FakeMessage actualMessage = mockMessageHandler.lastHandleMessage;
+		assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void notify_byDefault_returnsTrue() {
+		FakeMessageHandler mockMessageHandler = makeFakeMessageHandler();
+		Player stubPlayer = makePlayer();
+		FakeMessage stubMessage = makeFakeMessage();
+		MessageHandlerChainSet chain = makeChainSet();
+
+		chain.putHandler(FakeMessage.class, mockMessageHandler);
+
+		boolean result = chain.notify(stubPlayer, stubMessage);
+		assertTrue(result);
+	}
+
+	@Test
+	public void notify_whenCalledWithTerminatedMessage_doesNotNotifySecondHandlers() {
+		FakeMessageHandler stubMessageHandler = makeFakeMessageHandler();
+		FakeMessageHandler mockMessageHandler = makeFakeMessageHandler();
+		Player stubPlayer = makePlayer();
+		FakeMessage stubMessage = makeFakeMessage();
+		MessageHandlerChainSet chain = makeChainSet();
+
+		stubMessage.terminate();
+		chain.putHandler(FakeMessage.class, stubMessageHandler);
+		chain.putHandler(FakeMessage.class, mockMessageHandler);
+		chain.notify(stubPlayer, stubMessage);
+
+		Player expectedPlayer = null;
+		Player actualPlayer = mockMessageHandler.lastHandlePlayer;
+		assertEquals(expectedPlayer, actualPlayer);
+
+		FakeMessage expectedMessage = null;
+		FakeMessage actualMessage = mockMessageHandler.lastHandleMessage;
+		assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void notify_whenCalledWithTerminatedMessage_returnsFalse() {
+		FakeMessageHandler stubMessageHandler = makeFakeMessageHandler();
+		FakeMessageHandler mockMessageHandler = makeFakeMessageHandler();
+		Player stubPlayer = makePlayer();
+		FakeMessage stubMessage = makeFakeMessage();
+		MessageHandlerChainSet chain = makeChainSet();
+
+		stubMessage.terminate();
+		chain.putHandler(FakeMessage.class, stubMessageHandler);
+		chain.putHandler(FakeMessage.class, mockMessageHandler);
+
+		boolean result = chain.notify(stubPlayer, stubMessage);
+		assertFalse(result);
+	}
+
+	private Player makePlayer() {
+		return mock(Player.class);
+	}
+
+	private FakeMessage makeFakeMessage() {
+		return new FakeMessage();
+	}
+
+	private World makeWorld() {
+		return new World();
+	}
+
+	private FakeMessageHandler makeFakeMessageHandler() {
+		World world = makeWorld();
+		return new FakeMessageHandler(world);
+	}
+
+	private MessageHandlerChainSet makeChainSet() {
+		return new MessageHandlerChainSet();
+	}
+
+}
