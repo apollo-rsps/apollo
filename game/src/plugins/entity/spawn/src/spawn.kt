edea093deab70eef7a690e16d38c78be2ed861ff
Jesse <Jesse232@live.com>
2017-06-01 11:18:59 -0400

    Update Spawn.kt
    * I realized what saifix meant I believe

--- game/src/plugins/entity/spawn/src/spawn.kt
+++ game/src/plugins/entity/spawn/src/spawn.kt
@@ -1,6 +1,6 @@
 import org.apollo.game.model.*
 
-data class Spawn(val id: Int?, val name: String, val position: Position, val facing: Direction?,
+data class Spawn(val id: Int?, val name: String, val position: Position, val facing: Direction,
                  val spawnAnimation: Animation? = null,
                  val spawnGraphic: Graphic? = null)
 
@@ -8,6 +8,6 @@ object Spawns {
     val list = mutableListOf<Spawn>()
 }
 
-fun npc_spawn(name: String, x: Int, y: Int, z: Int = 0, id: Int? = -1, facing: Direction? = Direction.NORTH) {
+fun npc_spawn(name: String, x: Int, y: Int, z: Int = 0, id: Int? = null, facing: Direction = Direction.NORTH) {
     Spawns.list.add(Spawn(id, name, Position(x, y, z), facing))
 }
