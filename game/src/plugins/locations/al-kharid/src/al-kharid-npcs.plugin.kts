35ab034f3d6a677679a0ed990c25c0ca3047c310
Jesse <Jesse232@live.com>
2017-06-01 01:39:13 -0400

    Port location NPC spawns from Ruby to Kotlin

---
+++ game/src/plugins/locations/al-kharid/src/al-kharid-npcs.plugin.kts
@@ -0,0 +1,111 @@
+import org.apollo.game.model.Direction
+
+// Generic npcs
+
+npc_spawn("man", x = 3276, y = 3186)
+npc_spawn("man", x = 3282, y = 3197)
+
+npc_spawn("man", id = 3, x = 3301, y = 3200)
+npc_spawn("man", id = 3, x = 3300, y = 3208)
+
+npc_spawn("man", id = 2, x = 3297, y = 3196)
+
+npc_spawn("man", id = 16, x = 3294, y = 3204)
+
+npc_spawn("spider", x = 3319, y = 3145)
+npc_spawn("spider", x = 3319, y = 3140)
+npc_spawn("spider", x = 3323, y = 3138)
+
+npc_spawn("scorpion", x = 3282, y = 3149)
+
+//Camels
+
+npc_spawn("cam_the_camel", x = 3295, y = 3232)
+npc_spawn("elly_the_camel", x = 3312, y = 3210)
+npc_spawn("camel", x = 3285, y = 3198)
+npc_spawn("ollie_the_camel", x = 3291, y = 3209)
+npc_spawn("al_the_camel", x = 3275, y = 3162)
+
+// Quest npc
+
+npc_spawn("osman", x = 3286, y = 3180, facing = Direction.EAST)
+
+npc_spawn("hassan", x = 3302, y = 3163)
+
+npc_spawn("father_reen", x = 3272, y = 3158)
+
+npc_spawn("man", id = 663, x = 3297, y = 3287)
+
+// Boarder guards
+
+npc_spawn("border_guard", x = 3268, y = 3226)
+npc_spawn("border_guard", x = 3267, y = 3226)
+npc_spawn("border_guard", x = 3268, y = 3229, facing = Direction.SOUTH)
+npc_spawn("border_guard", x = 3267, y = 3229, facing = Direction.SOUTH)
+
+// Palace guards
+
+npc_spawn("Al-Kharid warrior", x = 3285, y = 3174)
+npc_spawn("Al-Kharid warrior", x = 3283, y = 3168)
+npc_spawn("Al-Kharid warrior", x = 3285, y = 3169)
+npc_spawn("Al-Kharid warrior", x = 3290, y = 3162)
+npc_spawn("Al-Kharid warrior", x = 3295, y = 3170)
+npc_spawn("Al-Kharid warrior", x = 3300, y = 3175)
+npc_spawn("Al-Kharid warrior", x = 3300, y = 3171)
+npc_spawn("Al-Kharid warrior", x = 3301, y = 3168)
+
+// Shanty pass
+
+npc_spawn("shantay_guard", x = 3301, y = 3120)
+npc_spawn("shantay_guard", x = 3302, y = 3126)
+npc_spawn("shantay_guard", x = 3306, y = 3126)
+npc_spawn("shantay_guard", id = 838, x = 3303, y = 3118)
+
+// Mine
+
+npc_spawn("scorpion", x = 3296, y = 3294)
+npc_spawn("scorpion", x = 3298, y = 3280)
+npc_spawn("scorpion", x = 3299, y = 3299)
+npc_spawn("scorpion", x = 3299, y = 3309)
+npc_spawn("scorpion", x = 3300, y = 3287)
+npc_spawn("scorpion", x = 3300, y = 3315)
+npc_spawn("scorpion", x = 3301, y = 3305)
+npc_spawn("scorpion", x = 3301, y = 3312)
+
+// Functional npcs
+
+npc_spawn("gnome_pilot", x = 3279, y = 3213)
+
+npc_spawn("banker", id = 496, x = 3267, y = 3164, facing = Direction.EAST)
+npc_spawn("banker", id = 496, x = 3267, y = 3167, facing = Direction.EAST)
+npc_spawn("banker", id = 496, x = 3267, y = 3169, facing = Direction.EAST)
+
+npc_spawn("banker", id = 497, x = 3267, y = 3166, facing = Direction.EAST)
+npc_spawn("banker", id = 497, x = 3267, y = 3168, facing = Direction.EAST)
+
+npc_spawn("gem_trader", x = 3287, y = 3210)
+
+npc_spawn("zeke", x = 3289, y = 3189)
+
+npc_spawn("shantay", x = 3304, y = 3124)
+
+npc_spawn("rug_merchant", id = 2296, x = 3311, y = 3109, facing = Direction.WEST)
+
+npc_spawn("ranael", x = 3315, y = 3163)
+
+npc_spawn("shop_keeper", id = 524, x = 3315, y = 3178)
+npc_spawn("shop_assistant", id = 525, x = 3315, y = 3180, facing = Direction.WEST)
+
+npc_spawn("louie_legs", x = 3316, y = 3175, facing = Direction.WEST)
+
+npc_spawn("ellis", x = 3274, y = 3192)
+
+npc_spawn("dommik", x = 3321, y = 3193)
+
+npc_spawn("tool_leprechaun", x = 3319, y = 3204)
+
+npc_spawn("ali_morrisane", x = 3304, y = 3211, facing = Direction.EAST)
+
+npc_spawn("silk_trader", x = 3300, y = 3203)
+
+npc_spawn("karim", x = 3273, y = 3180)
--- game/src/plugins/locations/lumbridge/src/lumbridge-npcs.plugin.kts
+++ game/src/plugins/locations/lumbridge/src/lumbridge-npcs.plugin.kts
@@ -5,6 +5,7 @@ npc_spawn("woman", id = 5, x = 3229, y = 2329)
 
 npc_spawn("hans", x = 3221, y = 3221)
 npc_spawn("father aereck", x = 3243, y = 3210)
+npc_spawn("bob", x = 3231, y = 3203)
 npc_spawn("shop keeper", x = 3212, y = 3247)
 npc_spawn("shop assistant", x = 3211, y = 3245)
 npc_spawn("lumbridge guide", x = 323, y = 3229)
