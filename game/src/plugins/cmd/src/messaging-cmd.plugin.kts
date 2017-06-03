5ea52e00f03d5faf63fbc4f3710ddc34657e4d9c
Cube <cubeee@users.noreply.github.com>
2017-06-03 19:30:22 +0300

    Convert messaging commands to Kotlin

---
+++ game/src/plugins/cmd/src/messaging-cmd.plugin.kts
@@ -0,0 +1,11 @@
+import org.apollo.game.model.entity.setting.PrivilegeLevel
+
+on_command("broadcast", PrivilegeLevel.ADMINISTRATOR)
+        .then { player ->
+            val message = arguments.joinToString(" ")
+            val broadcast = "[Broadcast] ${player.username.capitalize()}: $message"
+
+            player.world.playerRepository.forEach { other ->
+                other.sendMessage(broadcast)
+            }
+        }
