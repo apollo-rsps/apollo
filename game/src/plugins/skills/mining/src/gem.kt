c0433b9f1158c407fc053d51be80c10c8daeab17
Trevor Flynn <trevorflynn@liquidcrystalstudios.com>
2017-08-17 21:34:01 -0800

    Fix errors in gem.kt

--- game/src/plugins/skills/mining/src/gem.kt
+++ game/src/plugins/skills/mining/src/gem.kt
@@ -1,8 +1,8 @@
 
 
-data class Gem(id: Int, chance: Int)
+data class Gem(val id: Int, val chance: Int)
 
-val GEMS = Array(
+val GEMS = arrayOf(
         Gem(1623, 0), // uncut sapphire
         Gem(1605, 0), // uncut emerald
         Gem(1619, 0), // uncut ruby
