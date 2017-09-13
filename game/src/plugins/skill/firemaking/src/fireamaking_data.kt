78cde12186965aecd28aa76f5962548c8c7b5036
Trevor Flynn <trevorflynn@liquidcrystalstudios.com>
2017-09-13 12:23:28 -0700

    Code cleanup

--- game/src/plugins/skill/firemaking/src/fireamaking_data.kt
+++ game/src/plugins/skill/firemaking/src/fireamaking_data.kt
@@ -17,11 +17,4 @@ val FIRE_OBJ = 2732
 val ASH = 592
 val LIGHT_ANIMATION = Animation(733)
 
-fun lookupLog(id: Int): Log? {
-    for (log in Log.values()) {
-        if (log.id == id) {
-            return log;
-        }
-    }
-    return null
-}
+fun lookupLog(id: Int): Log? = Log.values().find { it.id == id }
