f6bf374c11351b7cec89b8e88a1f9a914bb4d29d
Trevor Flynn <trevorflynn@liquidcrystalstudios.com>
2017-11-05 16:25:31 -0800

    Add missing changes to git

--- game/src/main/java/org/apollo/game/release/r377/SendPlaySoundMessageEncoder.java
+++ game/src/main/java/org/apollo/game/release/r377/SendPlaySoundMessageEncoder.java
@@ -1,12 +1,10 @@
 package org.apollo.game.release.r377;
 
-import org.apollo.game.message.impl.SendFriendMessage;
 import org.apollo.game.message.impl.SendPlaySoundMessage;
 import org.apollo.net.codec.game.DataType;
 import org.apollo.net.codec.game.GamePacket;
 import org.apollo.net.codec.game.GamePacketBuilder;
 import org.apollo.net.release.MessageEncoder;
-import org.apollo.util.NameUtil;
 
 /**
  * A {@link MessageEncoder} for the {@link SendPlaySoundMessage}.
