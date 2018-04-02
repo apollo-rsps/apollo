559bef64f47601f7a11bd94212b6ab2d1700ed36
Major <major@emulate.rs>
2018-04-02 17:11:42 +0100

    Correct steel pickaxe level requirement

--- game/plugin/skills/mining/src/pickaxe.kt
+++ game/plugin/skills/mining/src/pickaxe.kt
@@ -7,7 +7,7 @@ import org.apollo.game.plugin.api.mining
 enum class Pickaxe(val id: Int, val level: Int, animation: Int, val pulses: Int) {
     BRONZE(id = 1265, level = 1, animation = 625, pulses = 8),
     ITRON(id = 1267, level = 1, animation = 626, pulses = 7),
-    STEEL(id = 1269, level = 1, animation = 627, pulses = 6),
+    STEEL(id = 1269, level = 6, animation = 627, pulses = 6),
     MITHRIL(id = 1273, level = 21, animation = 629, pulses = 5),
     ADAMANT(id = 1271, level = 31, animation = 628, pulses = 4),
     RUNE(id = 1275, level = 41, animation = 624, pulses = 3);
