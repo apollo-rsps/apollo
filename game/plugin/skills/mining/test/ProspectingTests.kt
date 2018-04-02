bbbc4497b3a915048d03e998a1cd17c6e5e7cfe2
Gary Tierney <gary.tierney@gmx.com>
2018-04-02 03:34:05 +0100

    Add tests for prospecting rocks

---
+++ game/plugin/skills/mining/test/ProspectingTests.kt
@@ -0,0 +1,46 @@
+
+import org.apollo.cache.def.ItemDefinition
+import org.apollo.game.plugin.skills.mining.Ore
+import org.apollo.game.plugin.testing.KotlinPluginTest
+import org.apollo.game.plugin.testing.actionCompleted
+import org.apollo.game.plugin.testing.ticks
+import org.junit.Test
+import org.junit.runners.Parameterized
+import org.mockito.Matchers.contains
+import org.powermock.modules.junit4.PowerMockRunnerDelegate
+
+data class ProspectingTestData(val rockId: Int, val expiredRockId: Int, val ore: Ore)
+
+@PowerMockRunnerDelegate(Parameterized::class)
+class ProspectingTests(private val data: ProspectingTestData) : KotlinPluginTest() {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() = Ore.values()
+            .flatMap { ore -> ore.objects.map { ProspectingTestData(it.key, it.value, ore) } }
+            .toList()
+    }
+
+    init {
+        val def = ItemDefinition(data.ore.id)
+        def.name = "<ore_type>"
+
+        items[data.ore.id] = def
+    }
+
+    @Test
+    fun `Prospecting a rock should reveal the type of ore it contains`() {
+        player.interactWithObject(data.rockId, 2)
+
+        verifyAfter(ticks(1), player) { sendMessage(contains("You examine the rock")) }
+        verifyAfter(actionCompleted(), player) { sendMessage(contains("This rock contains <ore_type>")) }
+    }
+
+    @Test
+    fun `Prospecting an expired rock should reveal it contains no ore`() {
+        player.interactWithObject(data.expiredRockId, 2)
+
+        verifyAfter(actionCompleted(), player) { sendMessage(contains("no ore available in this rock")) }
+    }
+}
