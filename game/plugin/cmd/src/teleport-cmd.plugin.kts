4125a151c033ce64ce7fa5e7845438121250592e
Caleb Foust <cfoust@sqweebloid.com>
2018-04-02 19:28:40 -0700

    Use find() and to

--- game/plugin/cmd/src/teleport-cmd.plugin.kts
+++ game/plugin/cmd/src/teleport-cmd.plugin.kts
@@ -12,36 +12,36 @@ on_command("pos", PrivilegeLevel.MODERATOR)
     }
 
 var TELEPORT_DESTINATIONS = listOf<Pair<String, Position>>(
-    Pair("ardougne", Position(2662, 3304, 0)),
-    Pair("barrows", Position(3565, 3314, 0)),
-    Pair("brimhaven", Position(2802, 3179, 0)),
-    Pair("burthorpe", Position(2898, 3545, 0)),
-    Pair("camelot", Position(2757, 3478, 0)),
-    Pair("canifis", Position(3493, 3489, 0)),
-    Pair("cw", Position(2442, 3090, 0)),
-    Pair("draynor", Position(3082, 3249, 0)),
-    Pair("duelarena", Position(3370, 3267, 0)),
-    Pair("edgeville", Position(3087, 3504, 0)),
-    Pair("entrana", Position(2827, 3343, 0)),
-    Pair("falador", Position(2965, 3379, 0)),
-    Pair("ge", Position(3164, 3476, 0)),
-    Pair("gwd", Position(2881, 5310, 2)),
-    Pair("kbd", Position(2273, 4680, 0)),
-    Pair("keldagrim", Position(2845, 10210, 0)),
-    Pair("kq", Position(3507, 9494, 0)),
-    Pair("lumbridge", Position(3222, 3219, 0)),
-    Pair("lunar", Position(2113, 3915, 0)),
-    Pair("misc", Position(2515, 3866, 0)),
-    Pair("neit", Position(2332, 3804, 0)),
-    Pair("pc", Position(2658, 2660, 0)),
-    Pair("rellekka", Position(2660, 3657, 0)),
-    Pair("shilo", Position(2852, 2955, 0)),
-    Pair("taverley", Position(2895, 3443, 0)),
-    Pair("tutorial", Position(3094, 3107, 0)),
-    Pair("tzhaar", Position(2480, 5175, 0)),
-    Pair("varrock", Position(3212, 3423, 0)),
-    Pair("yanille", Position(2605, 3096, 0)),
-    Pair("zanaris", Position(2452, 4473, 0))
+    "ardougne" to Position(2662, 3304, 0),
+    "barrows" to Position(3565, 3314, 0),
+    "brimhaven" to Position(2802, 3179, 0),
+    "burthorpe" to Position(2898, 3545, 0),
+    "camelot" to Position(2757, 3478, 0),
+    "canifis" to Position(3493, 3489, 0),
+    "cw" to Position(2442, 3090, 0),
+    "draynor" to Position(3082, 3249, 0),
+    "duelarena" to Position(3370, 3267, 0),
+    "edgeville" to Position(3087, 3504, 0),
+    "entrana" to Position(2827, 3343, 0),
+    "falador" to Position(2965, 3379, 0),
+    "ge" to Position(3164, 3476, 0),
+    "gwd" to Position(2881, 5310, 2),
+    "kbd" to Position(2273, 4680, 0),
+    "keldagrim" to Position(2845, 10210, 0),
+    "kq" to Position(3507, 9494, 0),
+    "lumbridge" to Position(3222, 3219, 0),
+    "lunar" to Position(2113, 3915, 0),
+    "misc" to Position(2515, 3866, 0),
+    "neit" to Position(2332, 3804, 0),
+    "pc" to Position(2658, 2660, 0),
+    "rellekka" to Position(2660, 3657, 0),
+    "shilo" to Position(2852, 2955, 0),
+    "taverley" to Position(2895, 3443, 0),
+    "tutorial" to Position(3094, 3107, 0),
+    "tzhaar" to Position(2480, 5175, 0),
+    "varrock" to Position(3212, 3423, 0),
+    "yanille" to Position(2605, 3096, 0),
+    "zanaris" to Position(2452, 4473, 0)
 )
 
 /**
@@ -54,19 +54,15 @@ on_command("tele", PrivilegeLevel.ADMINISTRATOR)
         if (arguments.size == 1) {
             val query = arguments[0]
 
-            for (target in TELEPORT_DESTINATIONS) {
-                val (name, dest) = target
-
-                if (!name.startsWith(query)) {
-                    continue
-                }
-
-                player.sendMessage("Teleporting to $name.")
-                player.teleport(dest)
+            val result = TELEPORT_DESTINATIONS.find { (name, _) -> name.startsWith(query) }
+            if (result == null) {
+                player.sendMessage("No destinations matching '$query'.")
                 return@then
             }
 
-            player.sendMessage("No destinations matching '$query'.")
+            val (name, dest) = result
+            player.sendMessage("Teleporting to $name.")
+            player.teleport(dest)
 
             return@then
         }
