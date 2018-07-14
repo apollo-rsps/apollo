b74be81f057517d07893ae996641ebb36da2f5ea
Steve Soltys <steve@stevesoltys.com>
2018-07-14 12:12:19 -0400

    Fix numerical attribute encoding

--- game/src/main/java/org/apollo/game/model/entity/attr/NumericalAttribute.java
+++ game/src/main/java/org/apollo/game/model/entity/attr/NumericalAttribute.java
@@ -30,13 +30,13 @@ public final class NumericalAttribute extends Attribute<Number> {
 
 	@Override
 	public byte[] encode() {
-		long encoded = type == AttributeType.DOUBLE ? Double.doubleToLongBits((double) value) : (long) value;
+		long encoded = type == AttributeType.DOUBLE ? Double.doubleToLongBits(value.doubleValue()) : value.longValue();
 		return Longs.toByteArray(encoded);
 	}
 
 	@Override
 	public String toString() {
-		return type == AttributeType.DOUBLE ? Double.toString((double) value) : Long.toString((long) value);
+		return type == AttributeType.DOUBLE ? Double.toString(value.doubleValue()) : Long.toString(value.longValue());
 	}
 
 }
