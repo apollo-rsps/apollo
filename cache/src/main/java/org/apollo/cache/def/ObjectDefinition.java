d578e8d153956fdae3e92fda7d96c6765b7d4e1c
Steve Soltys <steve@stevesoltys.com>
2018-07-30 22:26:30 -0400

    Add interaction mask decoding for GameObject definitions

--- cache/src/main/java/org/apollo/cache/def/ObjectDefinition.java
+++ cache/src/main/java/org/apollo/cache/def/ObjectDefinition.java
@@ -76,6 +76,11 @@ public final class ObjectDefinition {
 	private boolean impenetrable = true;
 
 	/**
+	 * A mask containing the set of directions from which this object can be interacted with.
+	 */
+	private int interactionMask;
+
+	/**
 	 * Denotes whether this object has actions associated with it or not.
 	 */
 	private boolean interactive;
@@ -135,6 +140,15 @@ public final class ObjectDefinition {
 	 */
 	public int getId() {
 		return id;
+	}
+
+	/**
+	 * Gets the interaction mask for this object.
+	 *
+	 * @return The interaction mask.
+	 */
+	public int getInteractionMask() {
+		return interactionMask;
 	}
 
 	/**
@@ -234,6 +248,15 @@ public final class ObjectDefinition {
 	 */
 	public void setInteractive(boolean interactive) {
 		this.interactive = interactive;
+	}
+
+	/**
+	 * Sets the interaction mask for this object.
+	 *
+	 * @param interactionMask The interaction mask.
+	 */
+	public void setInteractionMask(int interactionMask) {
+		this.interactionMask = interactionMask;
 	}
 
 	/**
