d4de32c82c7613e8528866116081887aab72337d
Major- <major@emulate.rs>
2018-08-20 20:41:27 +0100

    Rename npc_spawn function to spawnNpc

--- game/plugin/locations/lumbridge/src/npcs.plugin.kts
+++ game/plugin/locations/lumbridge/src/npcs.plugin.kts
@@ -1,15 +1,15 @@
 package org.apollo.plugin.locations.lumbridge
 
-import org.apollo.game.plugin.entity.spawn.npc_spawn
+import org.apollo.game.plugin.entity.spawn.spawnNpc
 
-npc_spawn("woman", id = 4, x = 3232, y = 3207)
-npc_spawn("man", id = 1, x = 3231, y = 3237)
-npc_spawn("man", id = 2, x = 3224, y = 3240)
-npc_spawn("woman", id = 5, x = 3229, y = 2329)
+spawnNpc("woman", id = 4, x = 3232, y = 3207)
+spawnNpc("man", id = 1, x = 3231, y = 3237)
+spawnNpc("man", id = 2, x = 3224, y = 3240)
+spawnNpc("woman", id = 5, x = 3229, y = 2329)
 
-npc_spawn("hans", x = 3221, y = 3221)
-npc_spawn("father aereck", x = 3243, y = 3210)
-npc_spawn("bob", x = 3231, y = 3203)
-npc_spawn("shop keeper", x = 3212, y = 3247)
-npc_spawn("shop assistant", x = 3211, y = 3245)
-npc_spawn("lumbridge guide", x = 323, y = 3229)
+spawnNpc("hans", x = 3221, y = 3221)
+spawnNpc("father aereck", x = 3243, y = 3210)
+spawnNpc("bob", x = 3231, y = 3203)
+spawnNpc("shop keeper", x = 3212, y = 3247)
+spawnNpc("shop assistant", x = 3211, y = 3245)
+spawnNpc("lumbridge guide", x = 323, y = 3229)
