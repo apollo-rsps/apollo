21ff6a34f03be13210e4975aaccd2fda30905848
Major- <major@emulate.rs>
2018-08-20 20:41:27 +0100

    Rename npc_spawn function to spawnNpc

--- game/plugin/entity/spawn/src/spawn.kt
+++ game/plugin/entity/spawn/src/spawn.kt
@@ -5,13 +5,19 @@ import org.apollo.game.model.Direction
 import org.apollo.game.model.Graphic
 import org.apollo.game.model.Position
 
-data class Spawn(val id: Int?, val name: String, val position: Position, val facing: Direction,
-                 val spawnAnimation: Animation? = null, val spawnGraphic: Graphic? = null)
-
-object Spawns {
-    val list = mutableListOf<Spawn>()
+fun spawnNpc(name: String, x: Int, y: Int, z: Int = 0, id: Int? = null, facing: Direction = Direction.NORTH) {
+    Spawns.list += Spawn(id, name, Position(x, y, z), facing)
 }
 
-fun npc_spawn(name: String, x: Int, y: Int, z: Int = 0, id: Int? = null, facing: Direction = Direction.NORTH) {
-    Spawns.list.add(Spawn(id, name, Position(x, y, z), facing))
+internal data class Spawn(
+    val id: Int?,
+    val name: String,
+    val position: Position,
+    val facing: Direction,
+    val spawnAnimation: Animation? = null,
+    val spawnGraphic: Graphic? = null
+)
+
+internal object Spawns {
+    val list = mutableListOf<Spawn>()
 }
--- game/plugin/locations/edgeville/src/npcs.plugin.kts
+++ game/plugin/locations/edgeville/src/npcs.plugin.kts
@@ -1,54 +1,54 @@
 package org.apollo.plugin.locations.edgeville
 
 import org.apollo.game.model.Direction
-import org.apollo.game.plugin.entity.spawn.npc_spawn
+import org.apollo.game.plugin.entity.spawn.spawnNpc
 
 // Generic npcs
 
-npc_spawn("man", x = 3095, y = 3508)
-npc_spawn("man", x = 3095, y = 3511)
-npc_spawn("man", x = 3098, y = 3509)
-npc_spawn("man", id = 2, x = 3093, y = 3511)
-npc_spawn("man", id = 3, x = 3097, y = 3508)
-npc_spawn("man", id = 3, x = 3092, y = 3508)
-npc_spawn("man", id = 3, x = 3097, y = 3512)
+spawnNpc("man", x = 3095, y = 3508)
+spawnNpc("man", x = 3095, y = 3511)
+spawnNpc("man", x = 3098, y = 3509)
+spawnNpc("man", id = 2, x = 3093, y = 3511)
+spawnNpc("man", id = 3, x = 3097, y = 3508)
+spawnNpc("man", id = 3, x = 3092, y = 3508)
+spawnNpc("man", id = 3, x = 3097, y = 3512)
 
-npc_spawn("guard", x = 3086, y = 3516)
-npc_spawn("guard", x = 3094, y = 3518)
-npc_spawn("guard", x = 3108, y = 3514)
-npc_spawn("guard", x = 3110, y = 3514)
-npc_spawn("guard", x = 3113, y = 3514)
-npc_spawn("guard", x = 3113, y = 3516)
+spawnNpc("guard", x = 3086, y = 3516)
+spawnNpc("guard", x = 3094, y = 3518)
+spawnNpc("guard", x = 3108, y = 3514)
+spawnNpc("guard", x = 3110, y = 3514)
+spawnNpc("guard", x = 3113, y = 3514)
+spawnNpc("guard", x = 3113, y = 3516)
 
-npc_spawn("sheep", id = 43, x = 3050, y = 3516)
-npc_spawn("sheep", id = 43, x = 3051, y = 3514)
-npc_spawn("sheep", id = 43, x = 3056, y = 3517)
-npc_spawn("ram", id = 3673, x = 3048, y = 3515)
+spawnNpc("sheep", id = 43, x = 3050, y = 3516)
+spawnNpc("sheep", id = 43, x = 3051, y = 3514)
+spawnNpc("sheep", id = 43, x = 3056, y = 3517)
+spawnNpc("ram", id = 3673, x = 3048, y = 3515)
 
-npc_spawn("monk", x = 3044, y = 3491)
-npc_spawn("monk", x = 3045, y = 3483)
-npc_spawn("monk", x = 3045, y = 3497)
-npc_spawn("monk", x = 3050, y = 3490)
-npc_spawn("monk", x = 3054, y = 3490)
-npc_spawn("monk", x = 3058, y = 3497)
+spawnNpc("monk", x = 3044, y = 3491)
+spawnNpc("monk", x = 3045, y = 3483)
+spawnNpc("monk", x = 3045, y = 3497)
+spawnNpc("monk", x = 3050, y = 3490)
+spawnNpc("monk", x = 3054, y = 3490)
+spawnNpc("monk", x = 3058, y = 3497)
 
 // Functional npcs
 
-npc_spawn("richard", x = 3098, y = 3516)
-npc_spawn("doris", x = 3079, y = 3491)
-npc_spawn("brother_jered", x = 3045, y = 3488)
-npc_spawn("brother_althric", x = 3054, y = 3504)
+spawnNpc("richard", x = 3098, y = 3516)
+spawnNpc("doris", x = 3079, y = 3491)
+spawnNpc("brother_jered", x = 3045, y = 3488)
+spawnNpc("brother_althric", x = 3054, y = 3504)
 
-npc_spawn("abbot_langley", x = 3059, y = 3484)
-npc_spawn("oziach", x = 3067, y = 3518, facing = Direction.EAST)
+spawnNpc("abbot_langley", x = 3059, y = 3484)
+spawnNpc("oziach", x = 3067, y = 3518, facing = Direction.EAST)
 
-npc_spawn("shop_keeper", id = 528, x = 3079, y = 3509)
-npc_spawn("shop_assistant", id = 529, x = 3082, y = 3513)
+spawnNpc("shop_keeper", id = 528, x = 3079, y = 3509)
+spawnNpc("shop_assistant", id = 529, x = 3082, y = 3513)
 
 
-npc_spawn("banker", x = 3096, y = 3489, facing = Direction.WEST)
-npc_spawn("banker", x = 3096, y = 3491, facing = Direction.WEST)
-npc_spawn("banker", x = 3096, y = 3492)
-npc_spawn("banker", x = 3098, y = 3492)
+spawnNpc("banker", x = 3096, y = 3489, facing = Direction.WEST)
+spawnNpc("banker", x = 3096, y = 3491, facing = Direction.WEST)
+spawnNpc("banker", x = 3096, y = 3492)
+spawnNpc("banker", x = 3098, y = 3492)
 
-npc_spawn("mage_of_zamorak", x = 3106, y = 3560)
+spawnNpc("mage_of_zamorak", x = 3106, y = 3560)
