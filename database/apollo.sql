ec93fdb0e5573013c598b8a310a89a9ebf455372
Sino <ilyas.alfan.b@gmail.com>
2020-05-30 00:27:24 +0200

    Apply feedback and expand

--- database/apollo.sql
+++ database/apollo.sql
@@ -1,83 +1,183 @@
+-- TODO: look into flyway-and liquibase to be able to get rid of these drops
+DROP PROCEDURE IF EXISTS create_new_account(p_email varchar, p_password varchar, p_rank rank);
+DROP PROCEDURE IF EXISTS create_new_stat(p_skill skill, p_stat integer, p_experience integer, p_owner varchar);
+DROP PROCEDURE IF EXISTS create_new_player(p_email varchar, p_display_name varchar, p_x integer, p_y integer, p_height integer);
+DROP PROCEDURE IF EXISTS create_new_appearance();
+DROP PROCEDURE IF EXISTS create_new_stat(p_skill skill, p_stat integer, p_experience integer, p_owner varchar);
+
 DROP TABLE IF EXISTS appearance;
-DROP TABLE IF EXISTS position;
 DROP TABLE IF EXISTS item;
-DROP TABLE IF EXISTS skill;
+DROP TABLE IF EXISTS stat;
 DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS account;
 
-DROP TYPE IF EXISTS gender_type;
+DROP TYPE IF EXISTS location;
+DROP TYPE IF EXISTS gender;
+DROP TYPE IF EXISTS rank;
+DROP TYPE IF EXISTS skill;
 
-CREATE TYPE privilege_level_type AS ENUM ('regular', 'pmod', 'staff');
-CREATE TYPE gender_type AS ENUM ('male', 'female');
+DROP DOMAIN IF EXISTS x_coord;
+DROP DOMAIN IF EXISTS y_coord;
+DROP DOMAIN IF EXISTS height_coord;
+
+CREATE TYPE rank AS ENUM ('player', 'moderator', 'administrator');
+CREATE TYPE gender AS ENUM ('male', 'female');
+
+CREATE DOMAIN x_coord AS int CHECK (VALUE >= 0 AND VALUE <= 16384);
+CREATE DOMAIN y_coord AS int CHECK (VALUE >= 0 AND VALUE <= 16384);
+CREATE DOMAIN height_coord AS int CHECK (VALUE >= 0 AND VALUE <= 3);
+
+CREATE TYPE location AS
+(
+    x      x_coord,
+    y      y_coord,
+    height height_coord
+);
+
+CREATE TYPE skill AS ENUM (
+    'attack',
+    'strength',
+    'defence',
+    'hitpoints',
+    'ranged',
+    'prayer',
+    'magic',
+    'cooking',
+    'fishing',
+    'woodcutting',
+    'firemaking',
+    'mining',
+    'smithing',
+    'agility',
+    'herblore',
+    'crafting',
+    'fletching',
+    'runecraft',
+    'slayer',
+    'farming',
+    'hunter',
+    'construction'
+    );
+
+CREATE TABLE account
+(
+    id       serial PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    rank     rank         NOT NULL
+);
 
 CREATE TABLE player
 (
-    id              serial PRIMARY KEY,
-    username        VARCHAR(255)         NOT NULL,
-    password        VARCHAR(255)         NOT NULL,
-    privilege_level privilege_level_type NOT NULL DEFAULT 'regular'
+    id           serial PRIMARY KEY,
+    display_name VARCHAR(15) NOT NULL,
+    location     location    NOT NULL,
+    account_id   integer references account (id)
 );
 
-CREATE UNIQUE INDEX player_username_index ON player (username);
+CREATE UNIQUE INDEX player_display_name_index ON player (display_name);
 
 CREATE TABLE appearance
 (
     id        serial PRIMARY KEY,
-    sex       gender_type       NOT NULL DEFAULT 'male',
-    styles    smallint ARRAY[7] NOT NULL DEFAULT '{ 0, 10, 18, 26, 33, 36, 42 }',
-    colors    smallint ARRAY[5] NOT NULL DEFAULT '{ 0, 0, 0, 0, 0 }',
-    player_id integer references player (id)
-);
-
-CREATE TABLE position
-(
-    id        serial PRIMARY KEY,
-    x         smallint NOT NULL CHECK (x >= 0 AND x <= 16384) DEFAULT 3222,
-    y         smallint NOT NULL CHECK (y >= 0 AND y <= 16384) DEFAULT 3222,
-    z         smallint NOT NULL CHECK (z >= 0 AND z <= 3)     DEFAULT 0,
+    gender    gender            NOT NULL,
+    styles    smallint ARRAY[7] NOT NULL,
+    colours   smallint ARRAY[5] NOT NULL,
     player_id integer references player (id)
 );
 
 CREATE TABLE item
 (
-    id           SERIAL PRIMARY KEY,
     inventory_id smallint CHECK (inventory_id >= 0),
     slot         smallint CHECK (slot >= 0),
     item_id      integer CHECK (item_id >= 0),
-    quantity     integer CHECK (quantity >= 0)
+    quantity     integer CHECK (quantity >= 0),
+    player_id    integer references player (id),
+    PRIMARY KEY (inventory_id, slot)
 );
 
-CREATE TABLE skill
+CREATE TABLE stat
 (
-    id         SERIAL PRIMARY KEY,
-    skill_id   smallint CHECK (skill_id >= 0),
+    skill      skill NOT NULL,
     stat       smallint CHECK (stat >= 0 AND stat <= 127),
-    experience float CHECK (experience >= 0 AND experience <= 200000000),
-    player_id  integer REFERENCES player (id)
+    experience integer CHECK (experience >= 0 AND experience <= 200000000),
+    player_id  integer REFERENCES player (id),
+    PRIMARY KEY (player_id, skill, stat)
 );
 
-CREATE OR REPLACE PROCEDURE create_new_player(p_username varchar, p_password varchar, p_x integer, p_z integer,
-                                              p_altitude integer)
+CREATE OR REPLACE PROCEDURE create_new_appearance(p_display_name varchar)
     LANGUAGE plpgsql
 AS
 $$
 BEGIN
-    INSERT INTO player (username, password)
-    VALUES (p_username, p_password);
-
-    INSERT INTO appearance (player_id)
-    VALUES ((SELECT id FROM player WHERE username = p_username));
-
-    INSERT INTO position (x, y, z, player_id)
-    VALUES (p_x, p_z, p_altitude, (SELECT id FROM player WHERE username = p_username));
-
-    FOR i IN 0..22
-        LOOP
-            INSERT INTO skill (skill_id, stat, experience, player_id)
-            VALUES (i, 1, 0, (SELECT id FROM player WHERE username = p_username));
-        END LOOP;
+    INSERT INTO appearance (gender, styles, colours, player_id)
+    VALUES ('male', '{ 0, 10, 18, 26, 33, 36, 42 }', '{ 0, 0, 0, 0, 0 }', (SELECT id FROM player WHERE display_name = p_display_name));
 
     COMMIT;
 END;
 $$;
 
-CALL create_new_player('Sino', 'hello123', 3254, 3420, 0);
+CREATE OR REPLACE PROCEDURE create_new_stat(p_skill skill, p_stat integer, p_experience integer, p_display_name varchar)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO stat (skill, stat, experience, player_id)
+    VALUES (p_skill, p_stat, p_experience, (SELECT id FROM player WHERE display_name = p_display_name));
+
+    COMMIT;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE create_new_account(p_email varchar, p_password varchar, p_rank rank)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO account (email, password, rank)
+    VALUES (p_email, p_password, p_rank);
+
+    COMMIT;
+END;
+$$;
+
+CREATE OR REPLACE PROCEDURE create_new_player(p_email varchar, p_display_name varchar, p_x integer, p_y integer, p_height integer)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    INSERT INTO player (display_name, location, account_id)
+    VALUES (p_display_name, ROW(p_x, p_y, p_height), (SELECT id FROM account WHERE email = p_email));
+
+    CALL create_new_appearance(p_display_name);
+
+    -- TODO: find a way to iterate through the possible values of an enum
+    CALL create_new_stat('attack', 1, 0, p_display_name);
+    CALL create_new_stat('strength', 1, 0, p_display_name);
+    CALL create_new_stat('defence', 1, 0, p_display_name);
+    CALL create_new_stat('hitpoints', 10, 1184, p_display_name);
+    CALL create_new_stat('ranged', 1, 0, p_display_name);
+    CALL create_new_stat('prayer', 1, 0, p_display_name);
+    CALL create_new_stat('magic', 1, 0, p_display_name);
+    CALL create_new_stat('cooking', 1, 0, p_display_name);
+    CALL create_new_stat('fishing', 1, 0, p_display_name);
+    CALL create_new_stat('woodcutting', 1, 0, p_display_name);
+    CALL create_new_stat('firemaking', 1, 0, p_display_name);
+    CALL create_new_stat('mining', 1, 0, p_display_name);
+    CALL create_new_stat('smithing', 1, 0, p_display_name);
+    CALL create_new_stat('agility', 1, 0, p_display_name);
+    CALL create_new_stat('herblore', 1, 0, p_display_name);
+    CALL create_new_stat('crafting', 1, 0, p_display_name);
+    CALL create_new_stat('fletching', 1, 0, p_display_name);
+    CALL create_new_stat('runecraft', 1, 0, p_display_name);
+    CALL create_new_stat('slayer', 1, 0, p_display_name);
+    CALL create_new_stat('farming', 1, 0, p_display_name);
+    CALL create_new_stat('hunter', 1, 0, p_display_name);
+    CALL create_new_stat('construction', 1, 0, p_display_name);
+
+    COMMIT;
+END;
+$$;
+
+CALL create_new_account('Sino@gmail.com', 'hello123', 'administrator');
+CALL create_new_player('Sino@gmail.com', 'Sino', 3254, 3420, 0);
