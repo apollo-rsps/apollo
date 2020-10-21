3558494850df10c9a9559892ed664ac86c437577
Grian <grianguy32@gmail.com>
2020-10-22 01:44:57 +0300

    mining: added gem chances

--- game/plugin/skills/mining/src/Gem.kt
+++ game/plugin/skills/mining/src/Gem.kt
@@ -1,10 +1,16 @@
 package org.apollo.game.plugin.skills.mining
 
-enum class Gem(val id: Int) { // TODO add gem drop chances
-    UNCUT_SAPPHIRE(1623),
-    UNCUT_EMERALD(1605),
-    UNCUT_RUBY(1619),
-    UNCUT_DIAMOND(1617);
+/* Chance info provided by https://oldschool.runescape.wiki/w/Gems
+ */
+
+enum class Gem(val id: Int,
+               val chance: Double,
+               val chanceWithGlory: Double
+) {
+    UNCUT_SAPPHIRE(1623, 0.00390625, 0.01162790698),
+    UNCUT_EMERALD(1605, 0.00390625, 0.01162790698),
+    UNCUT_RUBY(1619, 0.00390625, 0.01162790698),
+    UNCUT_DIAMOND(1617, 0.00390625, 0.01162790698);
 
     companion object {
         private val GEMS = Gem.values().associateBy({ it.id }, { it })
